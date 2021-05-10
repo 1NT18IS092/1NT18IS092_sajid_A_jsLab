29a7ff1a8e775c61ef6e8dfb0a8b22214f1c4aea
1NT18IS092 <82832378+1NT18IS092@users.noreply.github.com>
2021-05-11 02:31:48 +0530

    Add files via upload

---
+++ class 1 + 2/script.js
@@ -0,0 +1,10 @@
+var a = Math.floor(Math.random())* Math.floor(101);
+
+function check(){
+    var num=document.getElementById("num").value;
+    if(num==a){
+        alert("The number is correct");
+    }else if(num+10 < a){
+        alert("")
+    }else if
+}
